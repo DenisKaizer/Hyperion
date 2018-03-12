21d2af1abd4acf37f941801444fc50c75d48abe2
Alex <amijkko@yandex.ru>
2018-03-12 20:18:34 +0300

    Update Presale.sol

--- contracts/Presale.sol
+++ contracts/Presale.sol
@@ -86,7 +86,7 @@ contract Presale is Ownable, ReentrancyGuard {
     wallet = _wallet;
     token = HyperionWattToken(_token);
     whiteList = WhiteList(_whitelist);
-    hardCap = 23000000 * 1 ether; // inTokens
+    hardCap = 230000000 * 1 ether; // inTokens
     
   }
 
